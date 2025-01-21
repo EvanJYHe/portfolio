5ad9d761b2f0dd225991ef493ae4435829f9842d
Evan He <evan.he24@gmail.com>
2025-01-21 18:10:56 -0500

    added social links

--- src/components/Socials.tsx
+++ src/components/Socials.tsx
@@ -1,11 +1,38 @@
-
 const Socials: React.FC = () => {
     return (
-        <div className="text-2xl flex justify-center items-center gap-10 text-white">
-            <i className="fab fa-github"></i>
-            <i className="fab fa-linkedin"></i>
-            <i className="fa fa-envelope"></i>
-            <i className="fa fa-file"></i>
+        <div className="text-2xl flex justify-center items-center gap-10">
+            <a 
+                href="https://github.com/EvanJYHe" 
+                target="_blank" 
+                rel="noopener noreferrer"
+                aria-label="GitHub"
+            >
+                <i className="fab fa-github text-white"></i>
+            </a>
+            <a 
+                href="https://www.linkedin.com/in/evan-he-4253712a9/" 
+                target="_blank" 
+                rel="noopener noreferrer"
+                aria-label="LinkedIn"
+            >
+                <i className="fab fa-linkedin text-white"></i>
+            </a>
+            <a 
+                href="mailto:e35he@uwaterloo.ca" 
+                target="_blank" 
+                rel="noopener noreferrer"
+                aria-label="Email"
+            >
+                <i className="fa fa-envelope text-white"></i>
+            </a>
+            <a 
+                href="public/assets/Resume.pdf" 
+                target="_blank" 
+                rel="noopener noreferrer"
+                aria-label="Resume"
+            >
+                <i className="fa fa-file text-white"></i>
+            </a>
         </div>
     );
 };
